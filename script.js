a5f976cc02efb366c5a4ed661083fe092ee93b79
Balamiayush <aryanbalami54@gmail.com>
2025-01-04 14:43:58 +0545

    last commit of

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Initialize Lenis
+const lenis = new Lenis();
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+
 function updateDateTime() {
   const now = new Date();
   const date = now.toLocaleDateString(undefined, {
@@ -13,5 +22,4 @@ function updateDateTime() {
 }
 
 setInterval(updateDateTime, 1000);
-
 updateDateTime();
